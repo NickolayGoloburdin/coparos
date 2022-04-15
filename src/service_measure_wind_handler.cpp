34584caa1ab26b90895c09bbacc8973e5d1a26ff
nickolaygoloburdin <nickolaygoloburdin@mail.ru>
2022-04-15 11:35:22 +0300

    fix nh.set params

--- src/service_measure_wind_handler.cpp
+++ src/service_measure_wind_handler.cpp
@@ -54,8 +54,8 @@ private:
     auto action_result = ac.getResult();
     float speed = action_result->speed;
     float angle = action_result->angle;
-    n.setParam("/wind_speed", speed);
-    n.setParam("/wind_angle", angle);
+    n->setParam("/wind_speed", speed);
+    n->setParam("/wind_angle", angle);
 
     n->serviceClient<coparos::Service_command>("Continue");
     if (client_start.call(cmd)) {
