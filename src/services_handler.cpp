eec868322ccd1cca05073ec1c5cc107c5e5f66fc
nickolaygoloburdin <nickolaygoloburdin@mail.ru>
2022-04-28 17:30:11 +0300

    remove serial lib

--- src/services_handler.cpp
+++ src/services_handler.cpp
@@ -1,17 +1,16 @@
 #include "copa_types.h"
 #include "coparos/Service_command.h"
+#include "mavros_msgs/CommandBool.h"
+#include "mavros_msgs/CommandHome.h"
+#include "mavros_msgs/CommandInt.h"
+#include "mavros_msgs/CommandLong.h"
+#include "mavros_msgs/CommandTOL.h"
+#include "mavros_msgs/SetMode.h"
 #include "ros/ros.h"
-#include <coparos/Ack.h>
-#include <coparos/Command.h>
 // Класс для работы сервисов передачи команд и миссий в коптер
 class ServiceHandler {
 public:
-  ros::Publisher
-      cmd_pub_; //Издатель для отправки команд в модуль коммуникации с коптером
-  ros::NodeHandle *n; //Указатель на ноду РОС
-  coparos::Command
-      msg; //Сообщение РОС для хранения отпраленной на коптер команды
-  coparos::Ack ack_msg; //Сообщения для храниния ответа на команду с коптера
+  ros::NodeHandle *n;            //Указатель на ноду РОС
   ros::Subscriber ack_processor; // Подписчик на ответы с коптера
   ros::ServiceServer service_arm; //Сервис для снятия коптера с предохранителя
   ros::ServiceServer service_disarm; //Сервис для выключения двигателей
@@ -24,7 +23,6 @@ public:
   ros::ServiceServer service_clear_mission; //Сервис очистки миссии
   ServiceHandler(ros::NodeHandle *nh) : n(nh) {
     //Инициализация сервисо и подписчиков РОС
-    cmd_pub_ = n->advertise<coparos::Command>("/command", 1000);
     service_arm = n->advertiseService("Arm", &ServiceHandler::arm, this);
     service_disarm =
         n->advertiseService("Disarm", &ServiceHandler::disarm, this);
@@ -39,99 +37,65 @@ public:
         "Start_mission", &ServiceHandler::start_mission, this);
     service_clear_mission = n->advertiseService(
         "Clear_mission", &ServiceHandler::clear_mission, this);
-    ack_processor =
-        n->subscribe("/ack", 1000, &ServiceHandler::callback_ack, this);
   }
-  void callback_ack(const coparos::Ack msg) {
-    ack_msg = msg;
-  } //Функция обработчик ответа с коптера
+  //Функция обработчик ответа с коптера
   bool arm(coparos::Service_command::Request &req,
            coparos::Service_command::Response &res) {
-    //Каждый сервис работает по принципу
-    // 1. Отправил команду на коптер 2. Получил ответ об обработке команды 3.
-    // Вывел ответ об успешности выполнения команды
+
     ROS_INFO("ARMING");
-    msg.command = CMD_NAV_MOTORS_ON;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == CMD_NAV_MOTORS_ON) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
+    auto client_arm = n->serviceClient<mavros_msgs::CommandBool>("~cmd/arming");
+    mavros_msgs::CommandBool cmd;
+    cmd.request.value = true;
+    if (client_arm.call(cmd)) {
+      res.result = cmd.response.success;
+      return true;
+
     } else {
-      res.status = "Controller doesn't response";
+      res.status = "Cannot call mavros service";
       res.result = false;
       return true;
     }
+
     //   ROS_INFO("sending back response: [%ld]", (long int)res.sum);
   }
   bool disarm(coparos::Service_command::Request &req,
               coparos::Service_command::Response &res) {
     ROS_INFO("DISARMING");
-    msg.command = CMD_NAV_MOTORS_OFF;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == uint16_t(CMD_NAV_MOTORS_OFF)) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
+    auto client_disarm =
+        n->serviceClient<mavros_msgs::CommandBool>("~cmd/arming");
+    mavros_msgs::CommandBool cmd;
+    cmd.request.value = false;
+    if (client_disarm.call(cmd)) {
+      res.result = cmd.response.success;
+      return true;
+
     } else {
-      res.status = "Controller doesn't response";
+      res.status = "Cannot call mavros service";
       res.result = false;
       return true;
     }
   }
   bool takeoff(coparos::Service_command::Request &req,
                coparos::Service_command::Response &res) {
+    float takeoff_height;
+    if (!(n->getParam("/takeoff_height", takeoff_height))
+    {
+      res.status = "Cannot read takeoff height from rosparam";
+      res.result = false;
+      return true;
+    }
     ROS_INFO("TAKING OFF");
-    msg.command = CMD_NAV_TAKE_OFF;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == uint16_t(CMD_NAV_TAKE_OFF)) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
+    auto client_takeoff =
+        n->serviceClient<mavros_msgs::CommandTOL>("~cmd/takeoff");
+    mavros_msgs::CommandTOL cmd;
+
+    cmd.request.altitude = takeoff_height;
+    if (client_takeoff.call(cmd)) {
+      res.result = cmd.response.success;
+      return true;
+
     } else {
-      res.status = "Controller doesn't response";
+      res.status = "Cannot call mavros service";
       res.result = false;
       return true;
     }
@@ -139,28 +103,15 @@ public:
   bool land(coparos::Service_command::Request &req,
             coparos::Service_command::Response &res) {
     ROS_INFO("LANDING");
-    msg.command = CMD_NAV_TO_LAND;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == uint16_t(CMD_NAV_TO_LAND)) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
+    auto client_landing =
+        n->serviceClient<mavros_msgs::CommandTOL>("~cmd/land");
+    mavros_msgs::CommandTOL cmd;
+    if (client_landing.call(cmd)) {
+      res.result = cmd.response.success;
+      return true;
+
     } else {
-      res.status = "Controller doesn't response";
+      res.status = "Cannot call mavros service";
       res.result = false;
       return true;
     }
@@ -169,28 +120,17 @@ public:
   bool rtl(coparos::Service_command::Request &req,
            coparos::Service_command::Response &res) {
     ROS_INFO("Returning to launch");
-    msg.command = CMD_NAV_GOTO_HOME;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == uint16_t(CMD_NAV_GOTO_HOME)) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
+    auto client_rtl =
+        n->serviceClient<mavros_msgs::CommandLong>("~cmd/command");
+    mavros_msgs::CommandLong cmd;
+    cmd.request.broadcast = true;
+    cmd.request.command = uint16_t(20);
+    cmd.request.confirmation = 1;
+    if (client_rtl.call(cmd)) {
+      res.result = cmd.response.success;
+      return true;
     } else {
-      res.status = "Controller doesn't response";
+      res.status = "Cannot call mavros service";
       res.result = false;
       return true;
     }
@@ -198,28 +138,15 @@ public:
   bool stop(coparos::Service_command::Request &req,
             coparos::Service_command::Response &res) {
     ROS_INFO("Stopping");
-    msg.command = CMD_NAV_STOP_MOTION;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == uint16_t(CMD_NAV_STOP_MOTION)) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
+    auto client_stop = n->serviceClient<mavros_msgs::SetMode>("~/set_mode");
+    mavros_msgs::SetMode cmd;
+    cmd.request.base_mode = 1;
+    cmd.request.custom_mode = 4;
+    if (client_stop.call(cmd)) {
+      res.result = cmd.response.success;
+      return true;
     } else {
-      res.status = "Controller doesn't response";
+      res.status = "Cannot call mavros service";
       res.result = false;
       return true;
     }
@@ -227,28 +154,15 @@ public:
   bool continue_flight(coparos::Service_command::Request &req,
                        coparos::Service_command::Response &res) {
     ROS_INFO("Continue");
-    msg.command = CMD_NAV_CONTINUE_MOTION;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == uint16_t(CMD_NAV_CONTINUE_MOTION)) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
+    auto client_continue = n->serviceClient<mavros_msgs::SetMode>("~/set_mode");
+    mavros_msgs::SetMode cmd;
+    cmd.request.base_mode = 1;
+    cmd.request.custom_mode = 3;
+    if (client_continue.call(cmd)) {
+      res.result = cmd.response.success;
+      return true;
     } else {
-      res.status = "Controller doesn't response";
+      res.status = "Cannot call mavros service";
       res.result = false;
       return true;
     }
@@ -256,28 +170,37 @@ public:
   bool start_mission(coparos::Service_command::Request &req,
                      coparos::Service_command::Response &res) {
     ROS_INFO("Starting 1 click mission");
-    msg.command = CMD_EXEC_SWITCH_N;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == uint16_t(CMD_EXEC_SWITCH_N)) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
+
+    float takeoff_height;
+    if (!(n->getParam("/takeoff_height", takeoff_height))
+    {
+      res.status = "Cannot read takeoff height from rosparam";
+      res.result = false;
+      return true;
+    }
+    ROS_INFO("TAKING OFF");
+    auto client_takeoff =
+        n->serviceClient<mavros_msgs::CommandTOL>("~cmd/takeoff");
+    mavros_msgs::CommandTOL cmd;
+
+    cmd.request.altitude = takeoff_height;
+    if (client_takeoff.call(cmd)) {
+      res.result = cmd.response.success;
+
     } else {
-      res.status = "Controller doesn't response";
+      res.status = "Cannot call mavros service";
+      res.result = false;
+      return true;
+    }
+    auto client_continue = n->serviceClient<mavros_msgs::SetMode>("~/set_mode");
+    mavros_msgs::SetMode cmd;
+    cmd.request.base_mode = 1;
+    cmd.request.custom_mode = 3;
+    if (client_continue.call(cmd)) {
+      res.result = cmd.response.success;
+      return true;
+    } else {
+      res.status = "Cannot call mavros service";
       res.result = false;
       return true;
     }
@@ -285,31 +208,7 @@ public:
   bool clear_mission(coparos::Service_command::Request &req,
                      coparos::Service_command::Response &res) {
     ROS_INFO("Cleaning mission");
-    msg.command = CMD_NAV_CLEAR_WP;
-    cmd_pub_.publish(msg);
-    auto ack = ros::topic::waitForMessage<coparos::Ack>("/ack", *n,
-                                                        ros::Duration(0.1));
-    if (ack) {
-      if (ack->command == uint16_t(CMD_NAV_CLEAR_WP)) {
-        if (ack->result) {
-          res.status = "Success";
-          res.result = true;
-          return true;
-        } else {
-          res.status = ack->status;
-          res.result = false;
-          return true;
-        }
-      } else {
-        res.status = "Other command recieved";
-        res.result = false;
-        return true;
-      }
-    } else {
-      res.status = "Controller doesn't response";
-      res.result = false;
-      return true;
-    }
+    return true;
   }
 };
 int main(int argc, char **argv) {
