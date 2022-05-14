b6b900c1b1c97f63045d1985c0effed148dd55b1
nickolaygoloburdin <nickolaygoloburdin@mail.ru>
2022-05-14 11:36:04 +0300

    add baro_relative publishing

--- src/autostart_node.py
+++ src/autostart_node.py
@@ -2,50 +2,45 @@
 import roslaunch
 import rospy
 from std_msgs.msg import Float64, String
-
-
-uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-roslaunch.configure_logging(uuid)
-launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/jetson/copa5/ws/src/image_matching/image_processing/launch/find_pose.launch"])
 class Start:
-    def __init__(self,baseAltitude):
+    def __init__(self,baseAltitude, path):
         self.baseAltitude = baseAltitude
         self.status = False
-        # self.uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        # roslaunch.configure_logging(self.uuid)
-        # self.launch = roslaunch.parent.ROSLaunchParent(self.uuid, [path])
+        self.cb_status = False
+        self.uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(self.uuid)
+        self.launch = roslaunch.parent.ROSLaunchParent(self.uuid, [path])
         self.sub_altitude = rospy.Subscriber("/baro",Float64, self.gps_cb, queue_size=10)
         self.log_pub = rospy.Publisher("/logging_topic", String, queue_size=1)
-        self.baro_realtive_pub = rospy.Publisher("/baro_relative", Float64, queue_size=1)
     def gps_cb(self,data):
-        msg = Float64()
-        msg.data = data.data - self.baseAltitude
-        self.baro_realtive_pub.publish(msg)
         if abs(data.data - self.baseAltitude) > 20:
             self.setStatus(True)
         else:
             self.setStatus(False)
     def systemStart(self):
-        global launch
-        launch.start()
+        self.launch.start()
     def systemStop(self):
-        global launch
-        launch.shutdown()
+        self.launch.shutdown()
     def setStatus(self,state):
-        if self.status != state:
-            if state == True:
+        self.cb_status = state
+    def checkStatus(self):
+        if self.status != self.cb_status:
+            if self.cb_status == True:
                 self.systemStart()
             else:
                 self.systemStop()
-            self.status = state
+            self.status = self.cb_status
 #"/home/haier/catkin_ws/src/testapi/launch/test_node.launch"
 
 
 if __name__ == "__main__":
-
+    
     rospy.init_node('AutoStartNode', anonymous=True)
     baro = rospy.wait_for_message("/baro", Float64)
-    node = Start(baro.data)
+    node = Start(baro.data, "/home/jetson/copa5/ws/src/image_matching/image_processing/launch/find_pose.launch")
     rospy.loginfo("AutoStart enabled")
-    rospy.spin()
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        node.checkStatus()
+        rate.sleep()
 # 3 seconds later
