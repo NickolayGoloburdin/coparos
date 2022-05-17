abaadb0f9cf7401826a7a5357ce0febcf2fbc475
nickolaygoloburdin <nickolaygoloburdin@mail.ru>
2022-05-17 16:23:41 +0300

    add mission loading

--- src/mission_handler.py
+++ src/mission_handler.py
@@ -8,25 +8,46 @@ from std_msgs.msg import Int16
 
 
 class MissionPoint:
-    def __init__(self):
-        self.targetLat = 0
-        self.targetLon = 0
-        self.targetAlt = 0
-        self.targetRadius = 0
-        self.loiterTime = 0
-        self.maxHorizSpeed = 0
-        self.maxVertSpeed = 0
-        self.poiLat = 0
-        self.poiLon = 0
-        self.poiHeading = 0
-        self.poiAltitude = 0
-        self.flags = 0
-        self.photo = 0
-        self.panoSectorsCount = 0
-        self.panoDeltaAngle = 0
-        self.poiPitch = 0
-        self.poiRoll = 0
-        self.type = 0
+    def __init__(self, pList=None):
+        if pList == None:
+            self.targetLat = 0
+            self.targetLon = 0
+            self.targetAlt = 0
+            self.targetRadius = 0
+            self.loiterTime = 0
+            self.maxHorizSpeed = 0
+            self.maxVertSpeed = 0
+            self.poiLat = 0
+            self.poiLon = 0
+            self.poiHeading = 0
+            self.poiAltitude = 0
+            self.flags = 0
+            self.photo = 0
+            self.panoSectorsCount = 0
+            self.panoDeltaAngle = 0
+            self.poiPitch = 0
+            self.poiRoll = 0
+            self.type = 0
+        else:
+            self.targetLat = pList[0]
+            self.targetLon = pList[1]
+            self.targetAlt = pList[2]
+            self.targetRadius = pList[3]
+            self.loiterTime = pList[4]
+            self.maxHorizSpeed = pList[5]
+            self.maxVertSpeed = pList[6]
+            self.poiLat = pList[7]
+            self.poiLon = pList[8]
+            self.poiHeading = pList[9]
+            self.poiAltitude = pList[10]
+            self.flags = pList[11]
+            self.photo = pList[12]
+            self.panoSectorsCount = pList[13]
+            self.panoDeltaAngle = pList[14]
+            self.poiPitch = pList[15]
+            self.poiRoll = pList[16]
+            self.type = pList[17]
+
 # Класс для отправки полетных заданий в коптер
 
 
@@ -40,7 +61,7 @@ class MissionHandler:
         self.mission_request_handler = rospy.Subscriber(
             "/mission_request", Int16, self.handle_request)
         self.load_mission_service = rospy.Service(
-            "/LoadMissionFromJson", Load_mission, self.load_mission_fron_json)  # Сервис загрузки миссии из JSON
+            "/LoadMissionFromFile", Load_mission, self.load_mission_from_file)  # Сервис загрузки миссии из JSON
         self.reset_service = rospy.Service(
             "/SendMission", Service_command, self.send_mission)  # Сервис отправки миссии
         self.points = []
@@ -52,7 +73,7 @@ class MissionHandler:
             rospy.logerr("Mission is empty")
             return False
         if msg.data < len(self.points):
-            msg = create_msg_point(i)
+            msg = self.create_msg_point(i)
             self.pub.publish(msg)
 
     def create_msg_point(self, i):
@@ -84,16 +105,31 @@ class MissionHandler:
         if len(self.points) == 0:
             rospy.logerr("Mission is empty")
             return False
-        msg = create_msg_point(0)
+        msg = self.create_msg_point(0)
         self.pub.publish(msg)
 
-    def load_mission_fron_json(self, req):
-        return True
+    def load_mission_from_file(self, req):
+        file_name = "~/copa5/missions/{}.BIN".format(req.number)
+        dtype = np.dtype([('lat', np.float64), ('lon', np.float64), ('alt', np.float32), ('r', np.float32), ('time', np.int32), ('hs', np.float32),
+                          ('vs', np.float32), ('plat', np.float64), ('plon', np.float64), (
+                              'ph', np.float32), ('pa', np.float32), ('flags', np.uint32),
+                          ('photo', np.uint8), ('psc', np.uint8), ('pda', np.float32), ('pp', np.float32), ('pr', np.float32), ('typ', np.uint8)])
+
+        with open(file_name, "rb") as bin_file:
+            num_data = np.fromfile(bin_file, dtype)
+        list_num_data = num_data.tolist()
+        self.fill_mission_handler(list_num_data)
+        return Load_missionResponse(len(self.points))
+
+    def fill_mission_handler(self, pointList):
+        self.points.clear()
+        for i in pointList:
+            self.points.append(MissionPoint(i))
 
 
 if __name__ == '__main__':
     rospy.init_node('Mission_handler')
-    realtime = rospy.get_param("/realtime",False)
+    realtime = rospy.get_param("/realtime", False)
     if not realtime:
         exit()
     MissionHandler()
