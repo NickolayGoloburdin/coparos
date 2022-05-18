2cbe0cbe349cfe827d412a6883531ec75082d373
nickolaygoloburdin <nickolaygoloburdin@mail.ru>
2022-05-18 17:10:11 +0300

    fix services

--- src/services_handler.cpp
+++ src/services_handler.cpp
@@ -70,7 +70,7 @@ public:
     coparos::Command msg;
     msg.command = CMD_NAV_MOTORS_ON;
     cmd_pub_.publish(msg);
-    ros::spin();
+    ros::spinOnce();
     // ros::Duration(0.1).sleep();
     if (ack_.command == CMD_NAV_MOTORS_ON) {
       if (ack_.result) {
