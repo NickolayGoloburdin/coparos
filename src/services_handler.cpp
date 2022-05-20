9c4bceffad9efd669c3be3b98761dcec21a6fabf
nickolaygoloburdin <nickolaygoloburdin@mail.ru>
2022-05-20 17:13:59 +0300

    add delay service

--- src/services_handler.cpp
+++ src/services_handler.cpp
@@ -71,7 +71,7 @@ public:
     coparos::Command msg;
     msg.command = CMD_NAV_MOTORS_ON;
     cmd_pub_.publish(msg);
-    ros::Duration(2).sleep();
+    ros::Duration(0.1).sleep();
     ros::spinOnce();
     if (ack_.command == CMD_NAV_MOTORS_ON) {
       if (ack_.result) {
