16b22cd84e354929afebc1f84c64c8dad3056962
nickolaygoloburdin <nickolaygoloburdin@mail.ru>
2022-08-23 18:54:00 +0300

    add logging azimuth

--- src/state_machine_node.cpp
+++ src/state_machine_node.cpp
@@ -5,6 +5,7 @@
  *      Author: Nickolay
  */
 
+#include "copa_types.h"
 #include <actionlib/client/simple_action_client.h> // action Library Header File
 #include <actionlib/client/terminal_state.h> // Action Goal Status Header File
 #include <coparos/AzimuthFlyAction.h>
@@ -13,8 +14,11 @@
 #include <coparos/GPS.h>
 #include <coparos/MissionPoint.h>
 #include <coparos/Service_command.h>
+#include <cstdint>
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
+#include <string>
 
 // void write_callback(const std_msgs::String::ConstPtr &msg) {
 //   //   ROS_INFO_STREAM("Writing to serial port" << msg->data);
@@ -27,6 +31,7 @@ private:
   int channel11_, safety = 0;
   unsigned int drone_mode_, drone_prev_mode_, current_wp_;
   bool wind_is_measured = false;
+  uint16_t state_ = 0;
   // ros::Subscriber gnss_use_status_sub_;
   ros::Subscriber rc_channel_sub_;
   ros::ServiceClient flight_mode_service_client;
@@ -35,6 +40,8 @@ private:
   ros::ServiceClient missions_service_client;
   ros::ServiceClient measure_wind_service_client;
   std::vector<coparos::MissionPoint> mission_;
+  ros::Publisher log_pub_;
+  std_msgs::String log;
   // actionlib::SimpleActionClient<coparos::AzimuthFlyAction> ac;
 
   void callback_drone_info(const coparos::DroneInfo &msg) {
@@ -42,15 +49,21 @@ private:
     drone_mode_ = msg.DRONE_MODE;
     current_wp_ = msg.current_wp;
     gnss_status = msg.rc6_channel > 200 ? true : false;
+    state_ = msg.STATE;
   }
   // void callback_gnss(const std_msgs::Bool &msg) { gnss_status = msg.data; }
 
 public:
   void mes_wind() {
     if (channel11_ > 900) {
+      log.data = "Channel 11 = 1000; Starting measuring wind";
+      log_pub_.publish(log);
       coparos::Service_command cmd;
       measure_wind_service_client.call(cmd);
       wind_is_measured = true;
+      log.data = "Service " + std::to_string(cmd.response.result) +
+                 cmd.response.status;
+      log_pub_.publish(log);
     }
   }
   unsigned int create_target_flight_mode() {
@@ -78,20 +91,29 @@ public:
         nh_->serviceClient<coparos::Download_mission>("/GetMissionPointsList");
     measure_wind_service_client =
         nh_->serviceClient<coparos::Service_command>("/MeasureWind");
+    log_pub_ = nh_->advertise<std_msgs::String>("/logging_topic", 1000);
   }
   void set_target_mode() {
     coparos::Service_command cmd;
     unsigned int target = create_target_flight_mode();
-    if (target == 4 && target != current_mode() && safety < 20) {
+    if (target == 4 && target != current_mode() && safety < 20 &&
+        state_ == systemStateFlags::SYS_STATE_FLAG_FLYING) {
       cmd.request.param1 = 4;
       flight_mode_service_client.call(cmd);
       safety++;
-    } else if (target == 1 && target != current_mode() && safety < 20) {
+      log.data = "Set mission mode";
+      log_pub_.publish(log);
+    } else if (target == 1 && target != current_mode() && safety < 20 &&
+               state_ == systemStateFlags::SYS_STATE_FLAG_FLYING) {
       cmd.request.param1 = 1;
       flight_mode_service_client.call(cmd);
+      log.data = "Set althold mode";
+      log_pub_.publish(log);
       actionlib::SimpleActionClient<coparos::AzimuthFlyAction> ac("azimuth",
                                                                   true);
       ac.waitForServer();
+      log.data = "start azimuth fly";
+      log_pub_.publish(log);
       coparos::AzimuthFlyGoal goal;
       if (current_wp_ + 1 <= mission_.size()) {
         goal.target.targetLat = mission_[current_wp_ + 1].targetLat;
@@ -118,6 +140,8 @@ public:
   }
   void check_mission() {
     if (current_mode() == 4 && current_mode() != drone_prev_mode_) {
+      log.data = "Download mission from drone";
+      log_pub_.publish(log);
       coparos::Service_command srv;
       req_dwnld_mission_client.call(srv);
       coparos::Download_mission srvmission;
