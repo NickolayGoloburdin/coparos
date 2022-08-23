020f5aaeb32d1ae440f386286d9231d0b8645479
nickolaygoloburdin <nickolaygoloburdin@mail.ru>
2022-08-23 16:39:28 +0300

    fix mes wind

--- src/service_measure_wind_handler.cpp
+++ src/service_measure_wind_handler.cpp
@@ -69,7 +69,8 @@ private:
       return false;
     }
     auto action_result = 0; // ac.getResult();
-    float speed = action_result->speed;
+    float speed = 10;
+    // action_result->speed;
     float angle = 90.0 - radToDeg(action_result->angle) + heading_;
     n->setParam("/wind_speed", speed);
     n->setParam("/wind_angle", angle);
